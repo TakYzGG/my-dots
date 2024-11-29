5329e2fc249e9471a5fd2fbc0e87007db9265132
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2024-11-29 14:17:37 -0300

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -230,8 +230,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,			0,				Button1,	    cyclelayout,    {.i = +1 } },
+	{ ClkLtSymbol,			0,				Button3,	    cyclelayout,    {.i = -1 } },
 	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
