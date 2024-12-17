8dd6c4bb5591eb2d249e898ad5e3f781701abba3
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2024-12-17 12:13:59 -0300

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -10,9 +10,16 @@ picom &
 ./.fehbg
 unclutter &
 
-# Mouse / Trackpad
-synclient VertEdgeScroll=1 HorizEdgeScroll=1 TapButton1=1
-xinput set-prop 11 "Synaptics Move Speed" 5.0 5.0 0 0
+# Touchpad
+TOUCHPAD_NAME="SynPS/2 Synaptics TouchPad"
+TOUCHPAD_ID=$(xinput list | grep "$TOUCHPAD_NAME" | awk -F 'id=' '{print $2}' | awk '{print $1}')
+
+if [ -n "$TOUCHPAD_ID" ]; then
+    echo "Touchpad encontrado: $TOUCHPAD_NAME (ID: $TOUCHPAD_ID)"
+	# Configurar el touchpad
+	synclient VertEdgeScroll=1 HorizEdgeScroll=1 TapButton1=1
+	xinput set-prop "$TOUCHPAD_ID" "Synaptics Move Speed" 2.0 2.0 0 0
+fi
 
 # Verificar si el mouse Logitech USB está conectado
 MOUSE_NAME="Logitech USB Optical Mouse"
@@ -20,8 +27,9 @@ MOUSE_ID=$(xinput list | grep "$MOUSE_NAME" | awk -F 'id=' '{print $2}' | awk '{
 
 if [ -n "$MOUSE_ID" ]; then
     echo "Mouse encontrado: $MOUSE_NAME (ID: $MOUSE_ID)"
-    # Configurar la aceleración del mouse
+    # Configurar el mouse
     xinput set-prop "$MOUSE_ID" "libinput Accel Profile Enabled" 0, 1
+	xinput set-prop "$MOUSE_ID" "libinput Accel Speed" -0.1
 else
     echo "Mouse $MOUSE_NAME no encontrado. Configuración no aplicada."
 fi
