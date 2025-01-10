168932d2736825927fb2d1da4c182b887b339c65
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2025-01-10 10:30:13 -0300

    Add files via upload

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,21 @@
+-- Opciones Básicas
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.linespace = 0
+vim.opt.laststatus = 2
+
+-- No crear archivos de respaldo
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+-- Optimizaciones
+vim.opt.lazyredraw = true
+vim.opt.ttyfast = true
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 300
+vim.opt.history = 100
