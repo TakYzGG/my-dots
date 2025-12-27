ffd0927db47844e3ca25c5dc01978bea73333e96
takyz <test@test.com>
2025-12-27 14:46:47 -0300

    Crear script para autoinstalar mis configs y modificar el scripts de los wallpapers

---
+++ autoconfig.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Script para autoinstalar mis configuraciones
+
+# Crear carpetas si no existen
+mkdir -p ~/.config
+mkdir -p ~/.local/share/fonts/
+
+# Crear enlaces simbolicos
+ln -s ~/my-dots/configs/dwmblocks ~/.config/dwmblocks
+ln -s ~/my-dots/configs/lf ~/.config/lf
+ln -s ~/my-dots/configs/nvim ~/.config/nvim
+ln -s ~/my-dots/configs/picom ~/.config/picom
+ln -s ~/my-dots/configs/themes ~/.config/themes
+ln -s ~/my-dots/configs/scripts/dnemu ~/.config/menus
+ln -s ~/my-dots/wallpapers ~/Wallpapers
+
+# Mover las fuentes
+cp ~/my-dots/fonts/* ~/.local/share/fonts/
